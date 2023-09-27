4730b1d3c42b38c7256f5cdc0bdec771f1db16ea
zekeriya15 <dzikriansyah1503@gmail.com>
2023-09-27 09:32:17 +0700

    jurnal 01

---
+++ Jurnal01.java
@@ -0,0 +1,12 @@
+public class Jurnal01 {
+    public static void main (String[] args) {
+        String nama = "Muhamad Dzikriansyah" ;
+        String nim = "607062300103" ;
+        String kelas = "D3IF-47-04" ;
+
+        System.out.println ("Nama: " + nama) ;
+        System.out.println ("NIM: " + nim) ;
+        System.out.println ("Kelas: " + kelas) ;
+
+    }
+}
